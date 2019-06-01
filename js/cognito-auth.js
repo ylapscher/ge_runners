fe0ee34787cc3e78c9f7ea4ad1c6d16c4106623c
Samantha Streitman <samanthastreitman@Samanthas-MacBook-Pro.local>
2019-06-01 15:15:55 -0400

    changed redirect website

--- js/cognito-auth.js
+++ js/cognito-auth.js
@@ -135,7 +135,7 @@ var WildRydes = window.WildRydes || {};
             console.log('user name is ' + cognitoUser.getUsername());
             var confirmation = ('Registration successful. Please check your email inbox or spam folder for your verification code.');
             if (confirmation) {
-                window.location.href = 'verify.html';
+                window.location.href = 'index.html';
             }
         };
         var onFailure = function registerFailure(err) {
