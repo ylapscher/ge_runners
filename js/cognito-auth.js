3f2b72d51bad072e97da05cb3d9a7389c8cf9dd6
Samantha Streitman <samanthastreitman@Samanthas-MacBook-Pro.local>
2019-06-01 15:21:49 -0400

    error loging in

--- js/cognito-auth.js
+++ js/cognito-auth.js
@@ -117,7 +117,7 @@ var WildRydes = window.WildRydes || {};
         signin(email, password,
             function signinSuccess() {
                 console.log('Successfully Logged In');
-                window.location.href = 'ride.html';
+                window.location.href = 'index.html';
             },
             function signinError(err) {
                 alert(err);
