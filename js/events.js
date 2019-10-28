f72d1b8b0b321a2012d848587ad4cff8ed1660c2
Wilson <212582867@logon.ds.ge.com>
2019-10-28 01:49:24 -0400

    Added detailed info boxes for upcoming events

---
+++ js/events.js
@@ -0,0 +1,15 @@
+function makeInlineBlock(div) {
+    var x = document.getElementById(div);
+    x.style.display = "inline-block";
+}
+
+function makeBlock(div) {
+    var x = document.getElementById(div);
+    x.style.display = "block";
+}
+
+function hide(div) {
+    var x = document.getElementById(div);
+    x.style.display = "none";
+}
+
